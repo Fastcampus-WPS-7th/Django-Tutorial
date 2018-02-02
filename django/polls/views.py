e4d54d244dd495342c8510964dff0098ad7258ed
LeeHanYeong <dev@azelf.com>
2018-02-02 15:03:21 +0900

    index view가 템플릿을 사용하도록 변경

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
 from .models import Question
 
@@ -6,9 +8,21 @@ from .models import Question
 def index(request):
     # 가장 최근에 발행된 최대 5개의 Question목록
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # 쉼표단위로 구분된 Question목록의 각 항목의 question_text로 만들어진 문자열
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    # render를 사용
+    return render(request, 'polls/index.html', context)
+
+    # template을 명시적으로 불러와 rendering
+    # template = loader.get_template('poll/index.html')
+    # return HttpResponse(template.render(context, request))
+
+    # 쉼표단위로 구분된 Question목록의
+    #   각 항목의 question_text로 만들어진 문자열을 리턴
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
 
 
 def detail(request, question_id):
